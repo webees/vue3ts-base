15bf40c98afc8ff50693aaac562e9d36608be7be
webees <webees@qq.com>
2019-09-11 11:00:51 +0800

    # fix demo

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,17 @@
-var vueServe = require("./vue.serve");
-var vueBuild = require("./vue.build");
+var vueServe = require('./vue.serve')
+var vueBuild = require('./vue.build')
 
-const IS_DEV = process.env.NODE_ENV === "production" ? false : true;
-const CDN_BASE =
-  "//" +
-  process.env.VUE_APP_CDN_DOMAIN +
-  "/" +
-  process.env.VUE_APP_QINIU_PATH +
-  "/";
+const IS_DEV = process.env.NODE_ENV === 'production' ? false : true
+const CDN_BASE = '//' + process.env.VUE_APP_CDN_DOMAIN + '/' + process.env.VUE_APP_QINIU_PATH + '/'
 
 module.exports = {
-  publicPath: IS_DEV ? "/" : CDN_BASE,
+  publicPath: IS_DEV ? '/' : CDN_BASE,
   productionSourceMap: IS_DEV,
   integrity: true,
   css: {
     extract: false, // 不提取css
     sourceMap: IS_DEV,
-    loaderOptions: {
-      less: {
-        javascriptEnabled: true
-      }
-    }
+    loaderOptions: {}
   },
   devServer: {
     open: true,
@@ -29,15 +20,14 @@ module.exports = {
   },
   configureWebpack: {
     externals: {
-      "vue-router": "VueRouter",
-      "ant-design-vue": "Antd",
-      "vue-i18n": "VueI18n",
-      vue: "Vue",
-      vuex: "Vuex",
-      axios: "axios"
+      'vue-router': 'VueRouter',
+      'vue-i18n': 'VueI18n',
+      vue: 'Vue',
+      vuex: 'Vuex',
+      axios: 'axios'
     }
   },
   chainWebpack: webpackConfig => {
-    IS_DEV ? vueServe(webpackConfig) : vueBuild(webpackConfig);
+    IS_DEV ? vueServe(webpackConfig) : vueBuild(webpackConfig)
   }
-};
+}
